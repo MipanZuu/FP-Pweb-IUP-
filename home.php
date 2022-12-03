78963da77afae154d09217edc0aac4859a73e6d7
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2022-12-03 14:07:53 +0700

    testing home.css
    Co-Authored-By: M. Fachry Dwi Handoko <87126965+Delos-343@users.noreply.github.com>

--- home.php
+++ home.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Home | TEAM-X </title>
+    <!-- <link rel="stylesheet" href="./assets/styles/home.css"> -->
     <link rel="stylesheet" href="./assets/styles/home.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
